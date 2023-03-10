62642598fa70146125aace9c69ec0355b8343d8b
Kelvin Kellner <kelvinkellner123@gmail.com>
2023-03-09 19:08:57 -0500

    Create basic file structure

---
+++ src/templates/inc/menu.php
@@ -0,0 +1,6 @@
+<div id="menu">
+<ul>
+<li><a href="clients.php" <?php if($_GET['currentPage'] == 'clients') echo 'class="active"'; ?>>Clients</a></li>
+<li><a href="projects.php" <?php if($_GET['currentPage'] == 'projects') echo 'class="active"'; ?>>Projects</a></li>
+</ul>
+</div>
---
+++ src/templates/page.php
@@ -0,0 +1,15 @@
+<?php 
+include_once('../inc/head.php'); 
+?>
+
+<h1>My Classroom Grading Application</h1> 
+
+<?php include_once('../inc/menu.php'); ?> 
+
+<h3>Section Title</h3> 
+
+<?php 
+// Your main content goes here 
+?> 
+
+<?php include_once('../inc/footer.php'); ?>
