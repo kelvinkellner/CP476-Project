d3cd4543637786864d65e9f02abd396787371550
Kelvin Kellner <kelvinkellner123@gmail.com>
2023-03-20 21:00:14 -0400

    Remove unused code

--- src/auth/login.php
+++ src/auth/login.php
@@ -4,7 +4,6 @@ require_once(__DIR__.'/../db/use_db.php');
 if (isset($_SESSION['user'])) {
     echo "<h3>Welcome, {$_SESSION['user']['user_name']}</h3>";
 } else {
-    $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     echo <<<HTML
 <h3>Login</h3>
 <form action="/CP476/src/auth/handle_login.php" method="post">
