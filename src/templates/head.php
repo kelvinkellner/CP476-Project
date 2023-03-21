dd6ab90fd53579d66c1ec9c64c9120eacf98b16e
Kelvin Kellner <kelvinkellner123@gmail.com>
2023-03-21 03:05:19 -0400

    Add CSS stylesheet

--- src/templates/head.php
+++ src/templates/head.php
@@ -3,12 +3,13 @@
 <head>
 <title>My Classroom Grading Application</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link type="text/css" rel="Stylesheet" href="../css/style.css" />
+<?php include_once(__DIR__.'/style.php'); ?>
 </head>
 <body>
 <?php
 // Setup Session Storage
 session_start();
+
 // Setup Database
 include_once(__DIR__.'/../db/init_db.php');
 init_db();
---
+++ src/templates/style.php
@@ -0,0 +1,16 @@
+<style type="text/css">
+input.show-input-as-plain-text {
+    appearance: none;
+    border: none;
+    background: none;
+    padding: 0;
+    margin: 0;
+}
+th, td {
+    padding: 2px 4px;
+    text-align: left;
+}
+input, button {
+    margin: 1px 2px;
+}
+</style>
